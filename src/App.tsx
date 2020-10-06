bcb839a7fbcf84c4ff95bd11511ad104beb6ae01
Josh Mabry <mabry1985@gmail.com>
2020-10-06 16:03:05 -0700

    create json file, setup app component to parse data and set state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+import { createGlobalStyle } from "styled-components";
+import { BarterItem } from './types';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>BDO Dashboard</h1>
-    </div>
-  );
+import barterItemData from './barterItems.json';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #5a5a5a;
+  }
+`;
+
+type AppState = {
+  barterItems: BarterItem[];
+}
+
+class App extends Component<{}, AppState> {
+  constructor (props: {}) {
+    super(props)
+
+    this.state = {
+      barterItems: [], 
+    }
+  }
+
+  componentDidMount = () => {
+    const barterItemsCopy: BarterItem[] = [];
+   // barterItemData.items.map(barterItem => console.log(barterItem));
+    barterItemData.items.map(barterItem => barterItemsCopy.push(barterItem));
+    this.setState({ barterItems: barterItemsCopy }, 
+      () => console.log(this.state.barterItems))
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+          <div>
+            <h1>BDO Dashboard</h1>
+          </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type BarterItem = {
+  name: string
+  img: string
+  storage: Storage[]
+}
+
+export type Storage = {
+  name: string
+  count: string
+}
